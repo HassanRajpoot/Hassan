f74bd48a44f733b0929dcdf63958ddedbc96d436
HassanRajpoot <55589859+HassanRajpoot@users.noreply.github.com>
2019-09-20 18:53:56 +0500

    Add files via upload

--- main.c
+++ main.c
@@ -2,14 +2,11 @@
 
 int main()
 {
-    int p,h,f;
-    float k;
-	printf("enter the number of hours you work in a week\n");
-    scanf("%d",&h);
-    p = 500;
-    k =((h>0&&h>40)*.5)+1;
-    printf("%f\n",k);
-    f = h*k*p;
-    printf("%d",f);
+	int x;
+    int y;
+    printf("Enter leap year ");
+    scanf("%d",&x);
+    y = (x%4) == 0 && (x%100) ==0;
+    printf("%d\n",y);
 	return 0;
 }
